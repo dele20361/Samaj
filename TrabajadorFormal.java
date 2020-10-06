9c6f48e9ac83d1d9256a7da456f276dbf9dbbd4c
Pao <dele20361@uvg.edu.gt>
2020-10-06 14:06:03 -0600

    Se arregló el constructor y arreglaron parámetros de métodos

--- TrabajadorFormal.java
+++ TrabajadorFormal.java
@@ -11,56 +11,34 @@ Samaj-20
 public class TrabajadorFormal {
 
   //Atributos
-  private String Nombre;
-  private String Apellido;
   private int Edad;
-  private int Telefono;
-  private String Correo;
   private int NivelEducacion;
   private int Idiomas;
   private boolean Experiencia;
   private boolean Transporte;
   private boolean HomeOffice;
-  private int Codigo;
-  
-
-  public TrabajadorFormal( ) { }
-
   
   // Constructor
-  public TrabajadorFormal( String nom, String ape, int eda, int tel, String cor, int niv, int idi, boolean exp, boolean tra, boolean hom, int cod ) {
-    Nombre = nom;
-    Apellido = ape;
-    Edad = eda;
-    Telefono = tel;
-    Correo = cor;
-    NivelEducacion = niv;
-    Idiomas = idi;
+  public TrabajadorFormal( usuarioSamaj user, int Edad, int NivelEducacion, int Idioma, boolean exp, boolean Transporte, boolean HomeOffice ) {
+    super( user.getNombre(), user.getApellido(), user.getCorreo(), user.getTelefono(), user.getCodigo() );
+    Edad = Edad;
+    NivelEducacion = NivelEducacion;
+    Idiomas = Idioma;
     Experiencia = exp;
-    Transporte = tra;
-    HomeOffice = hom;
-    Codigo = cod;
+    Transporte = Transporte;
+    HomeOffice = HomeOffice;
   }
 
   
   //Getters
-  public String getNombre ( ) { return Nombre; }
-  public String getApellido ( ) { return Apellido; }
   public int getEdad ( ) { return Edad; }
-  public int getTelefono ( ) { return Telefono; }
-  public String getCorreo( ) { return Correo; }
   public boolean getExperiencia ( ) { return Experiencia; }
   public int getNivelEducacion ( ) { return NivelEducacion; }
   public int getIdiomas ( ) { return Idiomas; }
   public boolean getTransporte ( ) { return Transporte; }
   public boolean getHomeOffice ( ) { return HomeOffice; }
-  public int getCodigo ( ) { return Codigo; }
 
   //Setters
-  public void setNombre ( String Nombre ) { this.Nombre = Nombre; }
-  public void setApellido ( String Apellido ) { this.Apellido = Apellido; }
-  public void setTelefono ( int Telefono ) { this.Telefono = Telefono; }
-  public void setCorreo ( String Correo ) { this.Correo = Correo; }
   public void setNivelEducacion ( int NivelEducacion ) { this.NivelEducacion = NivelEducacion; }
   public void setIdiomas ( int Idiomas ) { this.Idiomas = Idiomas; }
   public void setExperiencia ( boolean Experiencia ) { this.Experiencia = Experiencia; }
@@ -70,20 +48,19 @@ public class TrabajadorFormal {
 
   /**
    * Método para mostrar la información de un trabajador informal.
-   * @param a Trabajador formal del cuál se quiere conocer la info.
    * @return String Con la información.
-   * @author 
+   * @author Paola De León
    */
-  public String InfoFormal ( TrabajadorFormal a ) {
+  public String toString ( ) {
 
     String mensaje = " Información de trabajador formal: \n";
-    mensaje += "\n- Nombre y apellido: " + a.getNombre () + " " + a.getApellido ();
-    mensaje += "\n- Edad: " + a.getEdad ();
-    mensaje += "\n- Telefono: " + a.getTelefono ();
-    mensaje += "\n- Correo: " + a.getCorreo ();
+    mensaje += "\n- Nombre y apellido: " + this.Nombre + " " + this.Apellido;
+    mensaje += "\n- Edad: " + this.Edad;
+    mensaje += "\n- Telefono: " + this.Telefono;
+    mensaje += "\n- Correo: " + this.Correo;
     
     //Se utiliza la informacion relevante al nivel de educacion
-    int NivelEducacion = a.getNivelEducacion ();
+    int NivelEducacion = this.NivelEducacion;
     switch ( NivelEducacion ) {
       case 1:
         mensaje += "\n- Nivel de educacion: Primaria";
@@ -95,7 +72,7 @@ public class TrabajadorFormal {
         mensaje += "\n- Nivel de educacion: Postgrado";
     }
     //Se utiliza la informacion relevante al idioma de conocimiento
-    int Idiomas = a.getIdiomas ();
+    int Idiomas = this.Idiomas;
     switch ( Idiomas ) {
       case 1:
         mensaje += "\n- Idioma de conocimiento: Español";
@@ -113,7 +90,7 @@ public class TrabajadorFormal {
         mensaje += "\n- Idiomas de conocimiento: Otros";
     }
 
-    Boolean Experiencia = a.getExperiencia();
+    boolean Experiencia = this.Experiencia;
     if ( Experiencia == true ) {
       mensaje += "\n- Experiencia: Sí";
     }
@@ -121,7 +98,7 @@ public class TrabajadorFormal {
       mensaje += "\n- Experiencia: No";
     }
 
-    Boolean Transporte = a.getTransporte();
+    Boolean Transporte = this.Transporte;
     if ( Transporte == true ) {
       mensaje += "\n- Transporte: Sí";
     }
@@ -129,7 +106,7 @@ public class TrabajadorFormal {
       mensaje += "\n- Transporte: No";
     }
 
-    Boolean HomeOffice = a.getHomeOffice();
+    boolean HomeOffice = this.HomeOffice;
     if ( HomeOffice == true ) {
       mensaje += "\n- Cuenta con herramientas para home office:: Sí";
     }
@@ -137,22 +114,10 @@ public class TrabajadorFormal {
       mensaje += "\n- Cuenta con herramientas para home office: No";
     }
 
-    mensaje += "\n- Codigo: " + a.getCodigo ();
+    mensaje += "\n- Codigo: " + this.Codigo;
+
 
     return mensaje;
   }
-
-  /**
-   * Método para subir el perfil del trabajador formal.
-   * @param TF Trababajador formal por agregar al ArrayList.
-   * @return void 
-   * @author
-  */
-  public void SubirPerfil ( TrabajadorFormal TF ) {
-    //Tipo Función
-    Documentos.AddTrabajadorFormal(TF);
-    //Se podría probar también ----> Documentos.obtenerPerfil(this.TrabajadorFormal); para obviar el argumento TrabajadorFormal TF.
-  }
-
 
 }
