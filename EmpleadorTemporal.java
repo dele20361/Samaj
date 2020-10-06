59b7c27cd27d2e7df77a6e050d8fc76e75bae5b2
Pao <dele20361@uvg.edu.gt>
2020-10-06 14:03:12 -0600

    Se modificó el nombre de la clase

---
+++ EmpleadorTemporal.java
@@ -0,0 +1,59 @@
+/*****************
+Proyecto Semestral
+Autor: Alejandro Gómez 20347, Marco Jurado 20308, Paola De Leon 20361, Andres Osorio, Alejandra Guzman 20262
+Fecha: 01/09/2020
+Ultima modificacion: 11/09/2020
+					
+Clase derivada de EmpleadorFormal.
+Samaj-20
+*****************/
+
+import java.util.ArrayList;
+
+public class EmpleadorTemporal extends EmpleadorFormal {
+    
+    // Propiedades
+    private ArrayList<TrabajoTemp> TrabajoTemporal; // Lista que almacenará los trabajos informales creados por el empleador.
+
+    //Constructor
+    public EmpleadorTemporal( usuarioSamaj user ) {
+        // Llamar a constructor de EmpleadorFormal
+        super( user.getNombre(), user.getApellido(), user.getCorreo(), user.getTelefono(), user.getCodigo() );
+        TrabajoInformal = new ArrayList<TrabajoTemp>(); 
+    }
+
+    // Getters
+    public ArrayList<TrabajoTemp> getTrabajoInformal() { return TrabajoInformal;}
+
+    // Setters
+    public ArrayList<TrabajoTemp> setTrabajoInformal ( ArrayList<TrabajoTemp> a ) { TrabajoInformal = a; return TrabajoInformal; }
+
+    /**
+     * Método para agregar un trabajo a la lista de trabajos temporales.
+     * @param a Trabajo temporal que se agregará.
+     * @return void
+     * @author Paola De León
+    */
+    public void AddTrabajoTemp ( TrabajoTemp a ) {
+        ArrayList<TrabajoTemp> ListaTrabajos = this.TrabajoTemporal;
+        ListaTrabajos.add ( a );
+    }
+
+
+    /**
+      * Método para mostrar la información de un empleador.
+      * @param a Empleador del cual se mostrará la información.
+      * @return String En el que se mostrará la infor.
+      * @author Paola De León
+    */
+    public String toString ( ) {
+        String mensaje = "";    
+        mensaje += "Informacion del empleador informal: ";  
+        mensaje += "- Nombre: " + this.Nombre;
+        mensaje += "- Codigo: " + this.Codigo;
+        mensaje += "- Contacto: " + this.Contacto;
+        
+        return mensaje;
+    }
+
+}
